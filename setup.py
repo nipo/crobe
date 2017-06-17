f9ab8aa7dfe27835219a176cd6400f85ac7ccb05
Nicolas Pouillon <nipo@ssji.net>
2017-06-17 16:37:49 +0200

    Add setuptools script

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "crobe",
+    version = "0.1",
+    description = "Generic probe toolset",
+    author = "Nicolas Pouillon",
+    author_email = "nipo@ssji.net",
+    license = "BSD",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+    ],
+    package_data = {
+        '': ['*.bit.gz'],
+    },
+    use_2to3 = False,
+    packages = find_packages(),
+    install_requires = ["pyelftools >= 0.23"],
+)
