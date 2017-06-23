a604e958c1d063503d1ebabcb65093873d0277f9
Nicolas Pouillon <nipo@ssji.net>
2017-06-23 13:54:10 +0200

    Mem-AP: Prefer self-incrementing mode, always rewrite CRW

--- crobe/component/arm/mem_ap.py
+++ crobe/component/arm/mem_ap.py
@@ -55,8 +55,8 @@ class MemAp(ap.Ap, model.Bus):
         be_to_size_l2 = {0xf: 2, 0x3: 1, 0xc: 1, 0x1: 0, 0x2: 0, 0x4: 0, 0x8: 0}
 
         address = None
-        csw_dirty = False
-        csw = 0
+        csw_dirty = True
+        csw = 0x012
 
         operations = []
 
