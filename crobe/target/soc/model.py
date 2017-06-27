7ef870bd8035140a9696e8e54d2eeffd82e038f0
Nicolas Pouillon <nipo@ssji.net>
2017-06-28 01:26:14 +0200

    soc: Add a generic loader op

--- crobe/target/soc/model.py
+++ crobe/target/soc/model.py
@@ -1,5 +1,6 @@
 from .. import model
 from ...part_id import PartId
+from ...memory import region
 
 __all__ = ["SoC"]
 
@@ -11,3 +12,13 @@ class SoC(model.Target):
     def __init__(self, name):
         model.Target.__init__(self, name)
         self.uid = 0
+
+    def load(self, program):
+        regions = self.children_of_class(region.Region)
+        for r in regions:
+            if not isinstance(r, region.Flash):
+                continue
+
+            pages = program.within(r.address, r.address + r.size)
+            r.erase(pages.address, pages.end - pages.address)
+            r.write(pages)
