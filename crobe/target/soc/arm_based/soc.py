01972224edd349dfac613bd706bc32c40e0dea89
Nicolas Pouillon <nipo@ssji.net>
2017-06-28 17:55:31 +0200

    arm SoC: Implement puppet

--- crobe/target/soc/arm_based/soc.py
+++ crobe/target/soc/arm_based/soc.py
@@ -7,9 +7,31 @@ from ....component.arm.cortex import Cortex
 from ....component.arm.sw_dp import SwDp
 from ....component.arm.jtag_dp import JtagDp
 from ....component.arm.mem_ap import MemAp
+from ....memory.region import Ram
+from ....puppet import Puppet
 from ....db import Db
 
-__all__ = ["SoC"]
+__all__ = ["SoC", 'ArmMPuppet']
+
+class ArmMPuppet(Puppet):
+    def __init__(self, soc):
+        cpu = soc.children_of_class(Cortex)[0]
+        ram = soc.children_of_class(Ram)[0]
+
+        Puppet.__init__(self, cpu, ram,
+                        pc_reg = cpu.registers[15],
+                        lr_reg = cpu.registers[14],
+                        sp_reg = cpu.registers[13],
+                        arg_regs = cpu.registers[:4],
+                        trampoline_code = b'\x10\xb5\x01L\xa0\x47\xbe\xbe',
+        )
+
+    def call(self, pc, *args):
+        self.prepare(pc, *args)
+        self.run()
+        self.wait()
+        r0 = self.arg_regs[0]
+        return self.cpu.reg_read([r0])[r0]
 
 class SoC(model.SoC):
     db = Db()
@@ -25,7 +47,10 @@ class SoC(model.SoC):
                 rt, = port.children_find(lambda x: isinstance(x, RomTable) and s in x.children)
                 self.child_add(Cortex.from_romtable(rt, idx))
                 idx += 1
-            
+
+    def puppet(self):
+        return ArmMPuppet(self)
+                
 @SoC.db.register_default
 def default_soc(ap):
     rom_tables = ap.children_of_class(RomTable)
