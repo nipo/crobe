ec0ddc375ea9946d9a6f8569976e9acae70701b0
Nicolas Pouillon <nipo@ssji.net>
2018-02-03 00:01:39 +0100

    setup.py: Put dependency_links to vhsic

--- setup.py
+++ setup.py
@@ -16,5 +16,8 @@ setup(
     },
     use_2to3 = False,
     packages = find_packages(),
-    install_requires = ["pyelftools >= 0.23", "vhsic"],
+    install_requires = ["pyelftools >= 0.23", "vhsic >= 0.1"],
+    dependency_links=[
+        'https://code.ssji.net/git/nipo/vhsic/snapshot/vhsic-c4430e08c1d3680606e01f42ad55bd0d67bb7f73.tar.gz#egg=vhsic-0.1'
+    ],
 )
