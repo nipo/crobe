c6edff2d32e928de2267a45e158943213721f407
Nicolas Pouillon <nipo@ssji.net>
2018-05-02 10:26:13 +0200

    lattice: Drop double enumeration
    Implement internal chain as it is done for proby: through
    another chain name rather than another enumerator

--- crobe/adapter/lattice.py
+++ crobe/adapter/lattice.py
@@ -7,24 +7,31 @@ __all__ = []
 # It looks like there are advanced features implemented in the Mach-XO, but undocumented.
 # Lattice was kind enough to actually let the FPGA in passthrough for JTAG by default.
 
-@model.Enumerator.register
-class Enumerator(basic.AdapterEnumerator):
+class Adapter(basic.Adapter):
+    supported_interfaces = ["jtag", "jtag-int"]
+
     def __init__(self):
         basic.AdapterEnumerator.__init__(self, "Lattice HW-USBN-2B",
                                        short_name = "lat",
                                        vid = 0x0403, pid = 0x6010,
                                        channel = "A")
 
-    def filter(self, adapter):
-        return adapter.device.vendor == "Lattice" and adapter.device.model.startswith("Lattice HW-USBN-2B")
+    def open(self, interface_name):
+        if interface_name == "jtag-int":
+            return basic.Adapter.open(self, "jtag", channel = "B")
+
+        elif interface_name == "jtag":
+            return basic.Adapter.open(self, "jtag", channel = "A")
 
 @model.Enumerator.register
 class Enumerator(basic.AdapterEnumerator):
+    adapter_class = Adapter
+
     def __init__(self):
-        basic.AdapterEnumerator.__init__(self, "Lattice HW-USBN-2B Internal Chain",
-                                       short_name = "int-lat",
+        basic.AdapterEnumerator.__init__(self, "Lattice HW-USBN-2B",
+                                       short_name = "lat",
                                        vid = 0x0403, pid = 0x6010,
-                                       channel = "B")
+                                       channel = "A")
 
     def filter(self, adapter):
         return adapter.device.vendor == "Lattice" and adapter.device.model.startswith("Lattice HW-USBN-2B")
