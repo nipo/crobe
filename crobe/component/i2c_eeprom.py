fabcb28c90b8563a4ecbc07a9caf7c43318fa6f5
Nicolas Pouillon <nipo@ssji.net>
2018-06-12 16:10:17 +0200

    i2c eeprom: Add support for i2c eeproms spanning multiple i2c addresses

--- crobe/component/i2c_eeprom.py
+++ crobe/component/i2c_eeprom.py
@@ -1,23 +1,35 @@
 from ..model import PortComponent
 from ..protocol import i2c
 import binascii
+import time
 
 __all__ = ["I2cEeprom"]
 
 class I2cEeprom(PortComponent):
-    def __init__(self, bus, saddr, addr_bytes = 2, size = None, page_size = None):
+    def __init__(self, bus, saddr, addr_bytes = 2, size = None, page_size = None, saddr_bits = 0):
         PortComponent.__init__(self, bus, "I2cEeprom")
         self.saddr = saddr
         self.addr_bytes = addr_bytes
-        self.size = 1 << (addr_bytes * 8) if size is None else size
-        self.page_size = self.size if page_size is None else page_size
+        self.saddr_bits = saddr_bits
+
+        max_size = 1 << (addr_bytes * 8 + saddr_bits)
+        self.size = size or max_size
+        self.page_size = page_size or self.size
+
+        if self.size > max_size:
+            raise ValueError("EEPROM size is more than accessible addresses")
+
+    def _addr(self, addr):
+        baddr = (addr & ((1 << (self.addr_bytes * 8)) - 1)).to_bytes(self.addr_bytes, 'big')
+        saddr = self.saddr + (addr >> (self.addr_bytes * 8))
+        return saddr, baddr
 
     def read(self, addr, size):
         assert addr + size <= self.size
 
-        baddr = addr.to_bytes(self.addr_bytes, 'big')
+        saddr, baddr = self._addr(addr)
 
-        return self.port.write_read(self.saddr, baddr, size)
+        return self.port.write_read(saddr, baddr, size)
 
     def write(self, addr, data):
         assert addr + len(data) <= self.size
@@ -35,13 +47,21 @@ class I2cEeprom(PortComponent):
     def _write(self, addr, data):
         assert 0 < len(data) <= self.page_size
 
-        addr = addr.to_bytes(self.addr_bytes, 'big')
-        return self.port.write(self.saddr, addr + data)
+        saddr, baddr = self._addr(addr)
+
+        deadline = time.time() + .1
+        while time.time() < deadline:
+            try:
+                return self.port.write(saddr, addr + data)
+            except i2c.AddressNack:
+                continue
 
     def option_set(self, opt):
         k, v = opt.split('=', 1)
         if k == 'saddr':
             self.saddr = int(v, 16)
+        elif k == 'saddr_bits':
+            self.saddr_bits = int(v, 16)
         elif k == 'addr_bytes':
             self.addr_bytes = int(v)
         elif k == 'size':
