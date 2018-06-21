9d047c3364bbd21a8e2952556c2341d23dbfabe7
Nicolas Pouillon <nipo@ssji.net>
2018-06-21 15:03:37 +0200

    i2c: Do probing by starting a 0-byte write

--- crobe/cli/info.py
+++ crobe/cli/info.py
@@ -62,7 +62,7 @@ def i2c_scan(roots, first, last, addr):
 
     for addr in addresses:
         try:
-            bus.read(addr, 1)
+            bus.write(addr, b'')
         except AddressNack:
             continue
         click.echo("Slave on address %02x" % addr)
