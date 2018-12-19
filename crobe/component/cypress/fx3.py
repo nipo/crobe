37d5c704fcbb9e45c658a2e31530bdbd4da0461a
Nicolas Pouillon <nipo@ssji.net>
2018-12-19 14:51:38 +0100

    fx3: Increase max JTAG freq

--- crobe/component/cypress/fx3.py
+++ crobe/component/cypress/fx3.py
@@ -4,7 +4,7 @@ from ...protocol import jtag
 @jtag.Tap.db.register(PartId(0, 0x34, 0x7926))
 class FX3(jtag.Tap):
     irlen = 4
-    max_freq = 10e6
+    max_freq = 25e6
 
     def __init__(self, port, index):
         jtag.Tap.__init__(self, port, index)
