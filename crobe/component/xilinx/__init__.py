abe09e679b9f7bf54a29f8a661dd0f6ce9198db7
Nicolas Pouillon <nipo@ssji.net>
2019-10-10 15:05:10 +0200

    xilinx: Add coolrunner2/spartan3

--- crobe/component/xilinx/__init__.py
+++ crobe/component/xilinx/__init__.py
@@ -1,3 +1,6 @@
 from . import spartan6
 from . import zynq
 from . import artix7
+from . import coolrunner2
+from . import spartan3
+from . import artix7
---
+++ crobe/component/xilinx/coolrunner2.py
@@ -0,0 +1,7 @@
+from ...part_id import PartId
+from ...protocol import jtag
+
+@jtag.Tap.db.register(PartId.from_idcode(0x06e59093).drop_revision())
+class Coolrunner2(jtag.Tap):
+    irlen = 8
+    max_freq = 33e6
