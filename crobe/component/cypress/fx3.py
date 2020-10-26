a75720bd1a5dc8ec8fe961081cf8adfb593fd38c
Nicolas Pouillon <nipo@ssji.net>
2020-10-26 12:17:30 +0100

    fx3: Add ARM9 CPU access

--- crobe/component/cypress/fx3.py
+++ crobe/component/cypress/fx3.py
@@ -1,36 +1,16 @@
 from ...part_id import PartId
+from ..arm.classic.jtag import Tap
+from ..arm.classic.embedded_ice import *
 from ...protocol import jtag
 
 @jtag.Tap.db.register(PartId(0, 0x34, 0x7926))
-class FX3(jtag.Tap):
-    irlen = 4
+class FX3(Tap):
     max_freq = 25e6
 
-    ARM_IDCODE  = jtag.Dr(32)
-    SCAN_PATH_SEL = jtag.Dr(5)
-    DEBUG_CHAIN = jtag.Dr(67)
-    EMBEDDEDICE_CHAIN = jtag.Dr(38)
-
-    IDCODE_ = jtag.Instruction(0xe, "DEVICE_ID")
-    SCAN_N = jtag.Instruction(0x2, "SCAN_PATH_SEL")
-    DEBUG = jtag.Instruction(0xc, "DEBUG_CHAIN")
-    EMBEDDEDICE = jtag.Instruction(0xc, "EMBEDDEDICE_CHAIN")
-    
     def __init__(self, port, index, idcode):
-        jtag.Tap.__init__(self, port, index, idcode)
-        self.name = "FX3"
-
-    def embedded_ice_read(self, reg):
-        cmds = [
-            self.SCAN_N.cmd(2, read_tdo = False),
-            self.cmd_run(1),
-            self.EMBEDDEDICE.cmd((1 << 37) | (reg << 32), read_tdo = False),
-            self.cmd_run(1),
-            self.EMBEDDEDICE.cmd((1 << 37) | (reg << 32), read_tdo = True),
-            ]
-        self.execute(cmds)
-        return cmds[-1].tdo & 0xffffffff
+        super().__init__(port, index, idcode, "FX3")
         
     def start(self):
-        for i in range(32):
-            self.logger.info("eice %d 0x%08x", i, self.embedded_ice_read(i))
+        self.dbg.debug_enable()
+        self.dbg.halt()
+        print(self.dbg.regs_read(range(15)))
