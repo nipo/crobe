729c4d227ee788702e0da43a6a51f26962f4992b
Nicolas Pouillon <nipo@ssji.net>
2020-12-18 16:55:21 +0100

    cli: Never hide unexpected exceptions

--- crobe/cli/console.py
+++ crobe/cli/console.py
@@ -17,45 +17,50 @@ def exc_pretty(e, pre = "", printer = print_va):
     else:
         printer("%sbecause of %s", pre, m)
     pre = pre + " "
+    once = False
     if isinstance(e, click.exceptions.BadParameter):
         p = e.param
         if isinstance(p, click.core.Option):
             printer("%sfor argument passed as %s" % (
                 pre, '/'.join(p.opts)))
+            once = True
 
     elif hasattr(e, "message_get"):
         for l in e.message_get().split("\n"):
             printer(pre+l)
+            once = True
 
     else:
         for l in traceback.format_exception(type(e), e, e.__traceback__):
+            once = True
             printer(pre + l.rstrip())
             
-        return
+        return once
 
     cause = e.__cause__
     if cause:
-        exc_pretty(cause, pre, printer)
+        return once or exc_pretty(cause, pre, printer)
         
 def cli():
+    ctx = None
     try:
         ctx = base.cli.make_context("crobe", sys.argv[1:])
         ctx.obj = {}
         base.cli.invoke(ctx)
 
     except click.exceptions.Exit as e:
-        if ctx.obj.get("raw_error", True):
-            raise
         sys.exit(e.exit_code)
         
     except Exception as e:
-        if ctx.obj.get("raw_error", True):
+        if not ctx or ctx.obj.get("raw_error", True):
             raise
         root = logging.getLogger('')
         output = print_va
         if root.level != 40:
             output = logging.getLogger('cli').critical
-        exc_pretty(e, printer = output)
+        prettified = exc_pretty(e, printer = output)
+        if not prettified:
+            raise
 
         try:
             ec = e.exit_code
