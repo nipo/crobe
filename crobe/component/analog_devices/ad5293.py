6ecd5d5cc1b1cd9749ea88c040060c4d44e9ad57
Nicolas Pouillon <nipo@ssji.net>
2020-12-20 17:22:27 +0100

    analog_devices/ad5293: Add read support

--- crobe/component/analog_devices/ad5293.py
+++ crobe/component/analog_devices/ad5293.py
@@ -16,13 +16,19 @@ class Ad5293(PortComponent):
     def __init__(self, port):
         super().__init__(port, "ad5293")
 
-    def opcode_send(self, opcode, arg):
+    def opcode_send(self, opcode, arg, read_miso = False):
         word = int(arg) | (int(opcode) << 10)
-        cmd = self.port.cmd_shift(word.to_bytes(2, "big"), read_miso = False)
+        cmd = self.port.cmd_shift(word.to_bytes(2, "big"), read_miso = read_miso)
         self.port.execute([self.port.cmd_cs(True), cmd, self.port.cmd_cs(False)])
+        if read_miso:
+            return int.from_bytes(cmd.miso, "big")
 
     def rdac_set(self, value):
         self.opcode_send(Opcode.Write, value)
+
+    def rdac_get(self):
+        self.opcode_send(Opcode.Read, 0)
+        return self.opcode_send(Opcode.Nop, 0)
 
     def control_set(self, calibration = False, rdac_protect = False):
         value = 0
@@ -31,6 +37,10 @@ class Ad5293(PortComponent):
         if not rdac_protect:
             value |= 0x2
         self.opcode_send(Opcode.ControlWrite, value)
+
+    def control_get(self):
+        self.opcode_send(Opcode.ControlRead, 0)
+        return self.opcode_send(Opcode.Nop, 0)
 
     def powerdown_set(self, shutdown = False):
         value = int(bool(shutdown))
