5925470e2dc7737e9929655902c57949b7ba2560
Nicolas Pouillon <nipo@ssji.net>
2020-12-21 12:06:36 +0100

    ad5293: Fix access

--- crobe/component/analog_devices/ad5293.py
+++ crobe/component/analog_devices/ad5293.py
@@ -16,35 +16,84 @@ class Ad5293(PortComponent):
     def __init__(self, port):
         super().__init__(port, "ad5293")
 
-    def opcode_send(self, opcode, arg, read_miso = False):
+    def write(self, opcode, arg = 0):
+        """
+        Write one opcode with argument.
+        Issues SDO release command after.
+        """
         word = int(arg) | (int(opcode) << 10)
-        cmd = self.port.cmd_shift(word.to_bytes(2, "big"), read_miso = read_miso)
-        self.port.execute([self.port.cmd_cs(True), cmd, self.port.cmd_cs(False)])
-        if read_miso:
-            return int.from_bytes(cmd.miso, "big")
+        cmd = self.port.cmd_shift(word.to_bytes(2, "big"), read_miso = True)
+        self.port.execute([
+            self.port.cmd_cs(True), cmd, self.port.cmd_cs(False),
+            self.port.cmd_cs(True), self.port.cmd_shift(b'\x80\x01', read_miso = False), self.port.cmd_cs(False),
+            self.port.cmd_cs(True), self.port.cmd_shift(b'\x00\x00', read_miso = False), self.port.cmd_cs(False),
+        ])
+
+    def read(self, opcode):
+        """
+        Read one opcode.
+        Issues SDO release command after.
+        """
+        word = int(opcode) << 10
+        w = self.port.cmd_shift(word.to_bytes(2, "big"), read_miso = True)
+        r = self.port.cmd_shift(b'\x00\x00', read_miso = True)
+        self.port.execute([
+            self.port.cmd_cs(True), w, self.port.cmd_cs(False),
+            self.port.cmd_cs(True), r, self.port.cmd_cs(False),
+            self.port.cmd_cs(True), self.port.cmd_shift(b'\x80\x01', read_miso = False), self.port.cmd_cs(False),
+            self.port.cmd_cs(True), self.port.cmd_shift(b'\x00\x00', read_miso = False), self.port.cmd_cs(False),
+        ])
+        return int.from_bytes(r.miso, "big") & 0x03ff
 
     def rdac_set(self, value):
-        self.opcode_send(Opcode.Write, value)
+        """
+        Sets RDAC register.
+
+        This does not unlock the RDAC. You should ensure it is writable.
+        """
+        self.write(Opcode.Write, value & 0x3ff)
 
     def rdac_get(self):
-        self.opcode_send(Opcode.Read, 0)
-        return self.opcode_send(Opcode.Nop, 0, read_miso = True)
+        """
+        Reads RDAC register.
+        """
+        return self.read(Opcode.Read)
 
     def control_set(self, calibration = False, rdac_protect = False):
+        """
+        Sets control bits.
+        """
         value = 0
         if calibration:
             value |= 0x4
         if not rdac_protect:
             value |= 0x2
-        self.opcode_send(Opcode.ControlWrite, value)
+        self.write(Opcode.ControlWrite, value)
 
     def control_get(self):
-        self.opcode_send(Opcode.ControlRead, 0)
-        return self.opcode_send(Opcode.Nop, 0)
+        """
+        Get control bits.
+        """
+        v = self.read(Opcode.ControlRead)
+        return dict(
+            calibration = bool(v & 0x4),
+            rdac_protect = not (v & 0x2),
+            )
 
     def powerdown_set(self, shutdown = False):
-        value = int(bool(shutdown))
-        self.opcode_send(Opcode.PowerDown, value)
+        """
+        Sets powerdown state.
+        """
+        self.write(Opcode.PowerDown, int(bool(shutdown)))
 
     def rdac_reset(self):
-        self.opcode_send(Opcode.Reset, 0)
+        """
+        Beware: this also reset the RDAC protect bit in control register
+        """
+        self.write(Opcode.Reset)
+
+    def nop(self):
+        """
+        Useless operation.
+        """
+        self.write(Opcode.Nop)
