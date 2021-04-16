8539ad33a3ded06f7f1573589af4003e4aae0d3c
Nicolas Pouillon <nipo@ssji.net>
2021-04-16 11:25:44 +0200

    protocol/base: Make power/reset options delayed to start()

--- crobe/protocol/base.py
+++ crobe/protocol/base.py
@@ -10,32 +10,35 @@ class Interface(model.PortComponent, FreqCapper):
     """
     Base class for protocol interfaces from an Adapter.
     """
+    power = None
+
     def __init__(self, port, name):
         self.do_reset = False
         model.PortComponent.__init__(self, port, name)
         FreqCapper.__init__(self)
         self._lock = threading.Lock()
-
+        self.__reset = None
+        self.__wait = 0
+        self.__do_power = None
+        
     def close(self):
         #self.port.child_remove(self)
         pass
 
     def option_set(self, opt):
         if opt == "reset":
-            self.do_reset = True
+            self.__reset = True
             return
 
         if opt.startswith("reset="):
-            self.reset(True)
-            w = float(sci_parse(opt[6:]))
-            time.sleep(w)
-            self.reset(False)
+            if opt[6:] == "hold":
+                self.__reset = "hold"
+                return
+            self.__reset = float(sci_parse(opt[6:]))
             return
 
         if opt.startswith("wait="):
-            w = float(sci_parse(opt[5:]))
-            self.logger.debug("Waiting %fs", w)
-            time.sleep(float(w))
+            self.__wait = float(sci_parse(opt[5:]))
             return
 
         if opt.startswith("fmax="):
@@ -44,11 +47,33 @@ class Interface(model.PortComponent, FreqCapper):
 
         if opt.startswith("power="):
             opt = opt[6:].lower()
-            self.power = opt in ["1", "on", "true"]
+            self.__do_power = opt in ["1", "on", "true"]
             return
 
-        model.PortComponent.option_set(self, opt)
+        super().option_set(opt)
 
+    def start(self):
+        if self.__do_power is not None:
+            self.power = self.__do_power
+
+        if self.__reset == "hold":
+            self.logger.info("Holding reset")
+            self.reset(True)
+        elif isinstance(self.__reset, float):
+            self.logger.info("Holding reset for %d sec", self.__reset)
+            self.reset(True)
+            time.sleep(self.__reset)
+            self.reset(False)
+        elif self.__reset is True:
+            self.logger.info("Cycling reset", self.__reset)
+            self.execute([self.cmd_reset(True), self.cmd_reset(False)])
+
+        if self.__wait:
+            self.logger.info("Waiting for %d sec", self.__wait)
+            time.sleep(self.__wait)
+
+        super().start()
+        
     def reset(self, asserted):
         return self.execute([self.cmd_reset(asserted)])
 
