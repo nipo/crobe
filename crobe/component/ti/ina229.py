77ca84fcd34e15bcf456ad7e84f4b5348a600af9
Nicolas Pouillon <nipo@ssji.net>
2021-06-17 18:03:53 +0200

    component/ti/ina229: Add register signedness info

--- crobe/component/ti/ina229.py
+++ crobe/component/ti/ina229.py
@@ -27,47 +27,51 @@ class Register(enum.IntEnum):
 @spi.Target.db.register("ina229")
 class Ina229(PortComponent):
     _register_size = {
-        Register.Config: 2,
-        Register.AdcConfig: 2,
-        Register.ShuntCalib: 2,
-        Register.ShuntTempCoef: 2,
-        Register.VShunt: 3,
-        Register.VBus: 3,
-        Register.Temperature: 2,
-        Register.Current: 3,
-        Register.Power: 3,
-        Register.Energy: 5,
-        Register.Charge: 5,
-        Register.DiagAlert: 2,
-        Register.ShuntOverVoltage: 2,
-        Register.ShuntUnderVoltage: 2,
-        Register.BusOverVoltage: 2,
-        Register.BusUnderVoltage: 2,
-        Register.TempLimit: 2,
-        Register.PowerLimit: 2,
-        Register.ManufId: 2,
-        Register.DeviceId: 2,
+        Register.Config: (2, False),
+        Register.AdcConfig: (2, False),
+        Register.ShuntCalib: (2, False),
+        Register.ShuntTempCoef: (2, False),
+        Register.VShunt: (3, False),
+        Register.VBus: (3, False),
+        Register.Temperature: (2, False),
+        Register.Current: (3, False),
+        Register.Power: (3, False),
+        Register.Energy: (5, False),
+        Register.Charge: (5, False),
+        Register.DiagAlert: (2, False),
+        Register.ShuntOverVoltage: (2, False),
+        Register.ShuntUnderVoltage: (2, False),
+        Register.BusOverVoltage: (2, False),
+        Register.BusUnderVoltage: (2, False),
+        Register.TempLimit: (2, False),
+        Register.PowerLimit: (2, False),
+        Register.ManufId: (2, False),
+        Register.DeviceId: (2, False),
     }
     
     def __init__(self, port):
         super().__init__(port, "ina229")
 
     def reg_read(self, reg):
-        reg_size = self._register_size[Register(reg)]
+        reg_size, signed = self._register_size[Register(reg)]
         cmd = self.port.cmd_shift(bytes([(int(reg) << 2) | 1]), read_miso = False)
         data = self.port.cmd_shift(b"\x00" * reg_size, read_miso = True)
 
         self.port.execute([self.port.cmd_cs(True), cmd, data, self.port.cmd_cs(False)])
 
-        return int.from_bytes(data.miso, "big")
+        return int.from_bytes(data.miso, "big", signed = signed)
 
     def reg_write(self, reg, value):
-        reg_size = self._register_size[Register(reg)]
-        blob = int(value).to_bytes(reg_size, "big")
+        reg_size, signed = self._register_size[Register(reg)]
+        blob = int(value).to_bytes(reg_size, "big", signed = signed)
 
         cmd = self.port.cmd_shift(bytes([int(reg) << 2]) + blob, read_miso = False)
 
         self.port.execute([self.port.cmd_cs(True), cmd, self.port.cmd_cs(False)])
+
+    def temperature_get(self):
+        v = self.reg_read(Register.Temperature)
+        return v/128
 
     def start(self):
         manufid = self.reg_read(Register.ManufId)
