0aa4ee2fc61c162df3abf801408767faea194fca
Nicolas Pouillon <nipo@ssji.net>
2021-06-17 17:43:13 +0200

    component/nsl/control_status: Add batched operation support

--- crobe/component/nsl/transactor/cs.py
+++ crobe/component/nsl/transactor/cs.py
@@ -9,8 +9,73 @@ class ControlStatus(PortComponent):
         super().__init__(route, name)
 
     def reg_read(self, no):
-        rsp = self.port.execute(bytes([self.CMD_READ | no]), 5)
-        return int.from_bytes(rsp[1:5], 'little')
+        r = self.cmd_reg_read(no)
+        self.execute([r])
+        return r.value
 
     def reg_write(self, no, value):
-        self.port.execute(bytes([self.CMD_WRITE | no]) + value.to_bytes(4, "little"), 1)
+        w = self.cmd_reg_write(no, value)
+        self.execute([w])
+
+    def reg_read_many(self, nos):
+        cmds = [self.cmd_reg_read(no) for no in nos]
+        self.execute(cmds)
+        return [op.value for op in cmds]
+
+    def reg_write_many(self, values):
+        cmds = [self.cmd_reg_write(addr, value) for (addr, value) in values.items()]
+        self.execute(cmds)
+
+    def reg_write(self, no, value):
+        w = self.cmd_reg_write(no, value)
+        self.execute([w])
+
+    def cmd_reg_read(self, no):
+        return StatusRead(no)
+
+    def cmd_reg_write(self, no, value):
+        return ControlWrite(no, value)
+
+    def execute(self, operation_list):
+        cmd = b""
+        rsp_size = 0
+
+        for o in operation_list:
+            if isinstance(o, StatusRead):
+                cmd += bytes([self.CMD_READ | o.reg])
+                rsp_size += 5
+            elif isinstance(o, ControlWrite):
+                cmd += bytes([self.CMD_WRITE | o.reg]) + int(o.value).to_bytes(4, "little")
+                rsp_size += 1
+            else:
+                raise NotImplementedError(o)
+
+        rsp = self.port.execute(cmd, rsp_size)
+
+        off = 0
+        for o in operation_list:
+            if isinstance(o, StatusRead):
+                off += 5
+                o.value = int.from_bytes(rsp[off+1:off+5], 'little')
+            else:
+                off += 1
+
+class Operation:
+    def __init__(self, reg):
+        self.reg = reg
+
+class StatusRead(Operation):
+    def __init__(self, reg):
+        super().__init__(reg)
+        self.value = None
+
+    def __str__(self):
+        return "<StatusRead 0x%02x>" % (self.reg,)
+        
+class ControlWrite(Operation):
+    def __init__(self, reg, value):
+        super().__init__(reg)
+        self.value = value
+
+    def __str__(self):
+        return "<ControlWrite 0x%02x 0x%08x>" % (self.reg, self.value)
