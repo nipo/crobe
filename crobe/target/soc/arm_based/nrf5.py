b1819d0d5b84a28c1bf559cd160b27b23159c257
Nicolas Pouillon <nipo@ssji.net>
2021-08-11 10:57:18 +0200

    target/nrf5x: Add nRF52811

--- crobe/target/soc/arm_based/nrf5.py
+++ crobe/target/soc/arm_based/nrf5.py
@@ -348,6 +348,13 @@ class nRF52840(nRF52):
     def __init__(self, dp):
         nRF52.__init__(self, "nRF52840", dp)
 
+@SoC.db.register(PartId(2, 0x44, 0xe))
+class nRF52820(nRF52):
+    GPIO_COUNT = 32
+
+    def __init__(self, dp):
+        nRF52.__init__(self, "nRF52811", dp)
+
 @SoC.db.register(PartId(2, 0x44, 0x10))
 class nRF52820(nRF52):
     GPIO_COUNT = 18
