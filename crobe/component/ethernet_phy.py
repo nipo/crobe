ec74704c19ec64585cd5bcf076b9c38be34d4a4e
Nicolas Pouillon <nipo@ssji.net>
2021-08-24 09:49:35 +0200

    component/ethernet_phy: Add more register definitions

--- crobe/component/ethernet_phy.py
+++ crobe/component/ethernet_phy.py
@@ -1,4 +1,5 @@
 from ..protocol import smi
+from .. import bitfield
 import enum
 
 class Register(enum.IntEnum):
@@ -19,6 +20,56 @@ class Register(enum.IntEnum):
     MmdAccessAddressData = 14
     ExtendedStatus = 15
 
+class Status(bitfield.Bitfield):
+    all = bitfield.Field(0, 16)
+    extended = bitfield.BooleanField(0)
+    jabber_detect = bitfield.BooleanField(1)
+    link = bitfield.BooleanField(2)
+    autoneg_able = bitfield.BooleanField(3)
+    remote_fault = bitfield.BooleanField(4)
+    autoneg_done = bitfield.BooleanField(5)
+    mf_pre_suppr = bitfield.BooleanField(6)
+    ext_status = bitfield.BooleanField(8)
+    tech_100bt2 = bitfield.BooleanField(9)
+    tech_100bt2_fd = bitfield.BooleanField(10)
+    tech_10bte = bitfield.BooleanField(11)
+    tech_10bte_fd = bitfield.BooleanField(12)
+    tech_100btx = bitfield.BooleanField(13)
+    tech_100btx_fd = bitfield.BooleanField(14)
+    tech_100bt4 = bitfield.BooleanField(15)
+
+class BasePage(bitfield.Bitfield):
+    all = bitfield.Field(0, 16)
+    selector = bitfield.Field(0, 5)
+    ability = bitfield.Field(5, 7)
+    t = bitfield.BooleanField(11)
+    xnp = bitfield.BooleanField(12)
+    rf = bitfield.BooleanField(13)
+    ack = bitfield.BooleanField(14)
+    np = bitfield.BooleanField(15)
+
+class MessagePage(bitfield.Bitfield):
+    all = bitfield.Field(0, 16)
+    message = bitfield.Field(0, 11)
+    t = bitfield.BooleanField(11)
+    # Can comply
+    ack2 = bitfield.BooleanField(12)
+    # Should be 1
+    mp = bitfield.BooleanField(13)
+    ack = bitfield.BooleanField(14)
+    np = bitfield.BooleanField(15)
+
+class UnformattedPage(bitfield.Bitfield):
+    all = bitfield.Field(0, 16)
+    u = bitfield.Field(0, 11)
+    t = bitfield.BooleanField(11)
+    # Can comply
+    ack2 = bitfield.BooleanField(12)
+    # Should be 0
+    mp = bitfield.BooleanField(13)
+    ack = bitfield.BooleanField(14)
+    np = bitfield.BooleanField(15)
+    
 @smi.Interface.db.register("eth_phy")
 class Clause22EthernetPhy(smi.C22Slave):
     def __init__(self, bus, name = "phy", phyad = None):
