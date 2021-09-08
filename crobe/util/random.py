d7259cf8531d451b491e5ec8c2b7aa7c52adb6a3
Nicolas Pouillon <nipo@ssji.net>
2021-09-08 13:45:54 +0200

    Add random generator util

---
+++ crobe/util/random.py
@@ -0,0 +1,8 @@
+__all__ = ["bitswap8"]
+
+def random_data(size):
+    with open("/dev/urandom", "rb") as fd:
+        ret = b''
+        while len(ret) < size:
+            ret += fd.read(size - len(ret))
+        return ret
