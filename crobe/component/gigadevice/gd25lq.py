773e5727d2ff942b15f9330ad11c95204264cf26
Nicolas Pouillon <nipo@ssji.net>
2022-02-04 17:41:05 +0100

    component/gigadevice: Add some info

--- crobe/component/gigadevice/gd25lq.py
+++ crobe/component/gigadevice/gd25lq.py
@@ -6,8 +6,13 @@ class Gd25lq(SfdpFlash):
     CMD_READ_UID = b"\x4b"
     write_buffer_size = 256
 
+    def __init__(self, port, idr):
+        super().__init__(port, idr)
+        self.logger.info("Is GD25LQ")
+
     def start(self):
         super().start()
-        print("Status: %02x %02x" % (self.status, self.command(b'\x35', rsize = 1)[0]))
+        self.logger.info("Status: %02x %02x" % (self.status, self.command(b'\x35', rsize = 1)[0]))
         self.uid = self.command(self.CMD_READ_UID, rsize = 8, dummy_words = 4)
         self.logger.info("Device UID: %s", self.uid.hex())
+        self.unprotect()
