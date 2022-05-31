1b20b4002fab9b2fc366552a457160c1de800e80
Nicolas Pouillon <nipo@ssji.net>
2022-05-31 18:50:50 +0200

    component/ethernet_phy: Add link status dumper

--- crobe/component/ethernet_phy.py
+++ crobe/component/ethernet_phy.py
@@ -88,3 +88,72 @@ class Clause22EthernetPhy(smi.C22Slave):
         else:
             return self.ext_read(0x1f, no)
 
+    def link_status_get(self):
+        bmcr = self.cmd_read(0)
+        bmsr = self.cmd_read(1)
+        anar = self.cmd_read(4)
+        lpar = self.cmd_read(5)
+        gsr = self.cmd_read(0xf)
+        gcr = self.cmd_read(0x9)
+        gst1 = self.cmd_read(0xa)
+
+        self.port.execute([bmcr, bmsr, gsr, gcr, anar, lpar, gst1])
+        bmsr = Status(all = bmsr.data)
+
+        local_support = set()
+        local_adv = set()
+        remote_adv = set()
+
+        for bit, token in {15:"100bt4", 14:"100btx-fd", 13:"100btx-hd", 12:"10bte-fd",
+                           11:"10bte-hd", 10:"100bt2-fd", 9:"100bt2-hd"}.items():
+            if bmsr.all & (1 << bit):
+                local_support.add(token)
+        if bmsr.all & 0x0100:
+            for bit, token in {15:"1000bx-fd", 14:"1000bx-hd", 13:"1000bt-fd", 12:"1000bt-hd"}.items():
+                if gsr.data & (1 << bit):
+                    local_support.add(token)
+
+        if not bmsr.link:
+            return "down"
+
+        if bmsr.autoneg_able and (bmcr.data & 0x1000):
+            for bit, token in {9:"100bt4", 8:"100btx-fd", 7:"100btx-hd", 6:"10bte-fd",
+                               5:"10bte-hd"}.items():
+                if anar.data & (1 << bit):
+                    local_adv.add(token)
+                if lpar.data & (1 << bit):
+                    remote_adv.add(token)
+            
+            for bit, token in {11:"1000bt-fd", 10:"1000bt-hd"}.items():
+                if gst1.data & (1 << bit):
+                    remote_adv.add(token)
+
+                if gcr.data & (1 << (bit-2)):
+                    local_adv.add(token)
+        else:
+            m0 = bool(bmcr.data & 0x2000)
+            m1 = bool(bmcr.data & 0x0040)
+            d = bool(bmcr.data & 0x0100)
+            if m1 and not m0:
+                if d:
+                    local_adv.add("1000bt-fd")
+                else:
+                    local_adv.add("1000bt-hd")
+            elif not m1 and m0:
+                if d:
+                    local_adv.add("100btx-fd")
+                else:
+                    local_adv.add("100btx-hd")
+            elif not m1 and not m0:
+                if d:
+                    local_adv.add("10bte-fd")
+                else:
+                    local_adv.add("10bte-hd")
+        common = local_support & local_adv & remote_adv
+        print(f"Local: {local_support}")
+        print(f"Local ADV: {local_adv}")
+        print(f"Remote ADV: {remote_adv}")
+        print(f"Common: {common}")
+
+        return common
+            
