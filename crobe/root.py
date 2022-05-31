f9548fe0df18035848b65e7d5822f57dea98054c
Nicolas Pouillon <nipo@ssji.net>
2022-05-31 18:45:20 +0200

    root: Allow to have / in root options

--- crobe/root.py
+++ crobe/root.py
@@ -1,11 +1,17 @@
 from .adapter.model import HwRoot
+import re
 
 def roots(paths):
     HwRoot.start_root()
 
+    splitter = re.compile(r'([a-z0-9._-]+(\([^\)]*\))?)', re.I)
+
     r = []
     for root in paths:
-        parts = root.split("/")
+        parts = splitter.split(root)
+        if not all((p in ["", "/"]) for p in parts[0::3]):
+            raise ValueError(root)
+        parts = parts[1::3]
         r.append(HwRoot.child_summon(*parts))
     return r
 
