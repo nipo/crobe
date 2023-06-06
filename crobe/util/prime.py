20b73d18610df2edfd95085c80fa999f36a1b0d0
Nicolas Pouillon <nipo@ssji.net>
2023-06-06 19:24:40 +0200

    util/prime: Add more prime utilities

--- crobe/util/prime.py
+++ crobe/util/prime.py
@@ -12,3 +12,33 @@ def prime_factors(n):
     if not factors:
         return [n]
     return factors
+
+def prime_map(n):
+    ret = {}
+    for i in prime_factors(int(n)):
+        if i in ret:
+            ret[i] += 1
+        else:
+            ret[i] = 1
+    return ret
+
+def integer_ratio(source, destination):
+    source_factors = prime_map(source)
+    destination_factors = prime_map(destination)
+
+    ratio_factors = dict(destination_factors.items())
+    for p, c in source_factors.items():
+        if p in ratio_factors:
+            ratio_factors[p] -= c
+        else:
+            ratio_factors[p] = -c
+
+    num = []
+    denom = []
+    for p, c in ratio_factors.items():
+        if c > 0:
+            num.extend([p] * c)
+        if c < 0:
+            denom.extend([p] * -c)
+
+    return num, denom
