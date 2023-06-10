b3f0cbbe19c4abe13df0ae3b0010a193fa46a9b0
Nicolas Pouillon <nipo@ssji.net>
2023-06-10 15:00:51 +0200

    firmware/flash/stubs/arm: Make register access volatile

--- firmware/flash/stubs/arm/common.h
+++ firmware/flash/stubs/arm/common.h
@@ -7,7 +7,7 @@ typedef unsigned char uint8_t;
 typedef unsigned long uintptr_t;
 typedef unsigned long size_t;
 
-#define REG32(x) *((uint32_t*)(x))
+#define REG32(x) *((volatile uint32_t*)(x))
 
 void flash_erase(uintptr_t addr, size_t bytes, size_t page_size);
 void flash_write(uintptr_t dest, const void *src, size_t bytes);
