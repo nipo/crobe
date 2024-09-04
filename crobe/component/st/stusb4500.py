3883e28e7d19fc83b3370cdd38fd2759393d4256
Nicolas Pouillon <nipo@ssji.net>
2024-09-04 09:14:20 +0200

    component/st4500: Enhance support

--- crobe/component/st/stusb4500.py
+++ crobe/component/st/stusb4500.py
@@ -1,34 +1,83 @@
 from ...model import PortComponent
 from ...protocol import i2c
+from ...bitfield import *
+import enum
 
-PASSWORD_REG_ADDR = 0x95
+class StUsb4500Register(enum.IntEnum):
+    PasswordReg = 0x95
+    Ctrl0       = 0x96
+    Ctrl1       = 0x97
+    RwBuffer    = 0x53
+
 FTP_CUST_PASSWORD     = 0x47
 
-CTRL_0_ADDR            = 0x96
-CTRL_0_PWR          = 0x80 
-CTRL_0_RST_N        = 0x40
-CTRL_0_REQ          = 0x10
-CTRL_0_SECT         = 0x07
+class Ctrl0(enum.IntFlag):
+    PWR          = 0x80 
+    RST_N        = 0x40
+    REQ          = 0x10
+    SECT         = 0x07
 
-CTRL_1_ADDR            = 0x97
-CTRL_1_SECT_MAP          = 0xF8
-CTRL_1_OPCODE       = 0x07
-RW_BUFFER_ADDR             = 0x53
+class Ctrl1Op(enum.IntEnum):
+    READ             = 0x00
+    WRITE_PL         = 0x01
+    WRITE_SER        = 0x02
+    READ_PL          = 0x03
+    READ_SER         = 0x04
+    ERASE_SECTOR     = 0x05
+    PROG_SECTOR      = 0x06
+    SOFT_PROG_SECTOR = 0x07
 
-OP_READ             = 0x00
-OP_WRITE_PL         = 0x01
-OP_WRITE_SER        = 0x02
-OP_READ_PL          = 0x03
-OP_READ_SER         = 0x04
-OP_ERASE_SECTOR     = 0x05
-OP_PROG_SECTOR      = 0x06
-OP_SOFT_PROG_SECTOR = 0x07
-SECTOR_0 = 0x01
-SECTOR_1 = 0x02
-SECTOR_2 = 0x04
-SECTOR_3 = 0x08
-SECTOR_4 = 0x10
+class Ctrl1Sector(enum.IntFlag):
+    SECTOR_0 = 0x01 << 3
+    SECTOR_1 = 0x02 << 3
+    SECTOR_2 = 0x04 << 3
+    SECTOR_3 = 0x08 << 3
+    SECTOR_4 = 0x10 << 3
 
+class Bank0(Bitfield):
+    all = Field(0, 64)
+    VendorId = Field(0, 16)
+    ProductId = Field(16, 16)
+    BcdDevice = Field(32, 16)
+    PortRoleCtrl = Field(48, 8)
+    DevicePowerRoleCtrl = Field(56, 8)
+
+class Bank1(Bitfield):
+    all = Field(0, 64)
+    GpioCfg = Field(2, 2)
+    BusDchrg = BooleanField(13)
+    R1 = BooleanField(14)
+    R0 = BooleanField(15)
+    VbusDischTimeToPdo = Field(16, 4)
+    DischTimeTo0V = Field(20, 4)
+
+class Bank2(Bitfield):
+    all = Field(0, 64)
+
+class Bank3(Bitfield):
+    all = Field(0, 64)
+    UsbCommCapable = BooleanField(16)
+    PdmSinkPdoNumb = Field(17, 2)
+    SinkUnconsPower = BooleanField(18)
+    LutSinkPdo1 = Field(20, 4)
+    SinkLL1 = Field(24, 4)
+    SinkHL1 = Field(28, 4)
+    LutSinkPdo2 = Field(32, 4)
+    SinkLL2 = Field(36, 4)
+    SinkHL2 = Field(40, 4)
+    LutSinkPdo3 = Field(44, 4)
+    SinkLL3 = Field(48, 4)
+    SinkHL3 = Field(52, 4)
+
+class Bank4(Bitfield):
+    all = Field(0, 64)
+    SinkPdoFlex1V = Field(6, 10)
+    SinkPdoFlex2V = Field(16, 10)
+    SinkPdoFlexI = Field(26, 10)
+    PowerOkCfg = Field(37, 2)
+    ReqSrcCurrent = BooleanField(52)
+    AlertStatus1Mask = Field(56, 8)
+    
 @i2c.Interface.db.register("stusb4500")
 class StUsb4500(i2c.Slave):
     def __init__(self, bus, saddr = 0x32):
@@ -36,67 +85,90 @@ class StUsb4500(i2c.Slave):
         self.saddr = saddr
 
     def write(self, addr, data):
+        #self.logger.debug("%s < %s", addr.name, data.hex())
         super().write(bytes([addr]) + data)
 
     def read(self, addr, size):
-        return super().write_read(bytes([addr]), size)
+        r = super().write_read(bytes([addr]), size)
+        #self.logger.debug("%s > %s", addr.name, r.hex())
+        return r
 
     def ftp_password(self, value = 0):
-        self.write(PASSWORD_REG_ADDR, bytes([value]))
+        self.write(StUsb4500Register.PasswordReg, bytes([value]))
 
     def ftp_ctrl0(self, value = 0):
-        self.write(CTRL_0_ADDR, bytes([value]))
+        self.logger.debug("Ctrl0 < %s", repr(value))
+        self.write(StUsb4500Register.Ctrl0, bytes([value]))
+
+    def ftp_buffer_write(self, data):
+        self.logger.debug("Buffer < %s", data.hex())
+        self.write(StUsb4500Register.RwBuffer, data)
+
+    def ftp_buffer_read(self):
+        r = self.read(StUsb4500Register.RwBuffer, 8)
+        self.logger.debug("Buffer > %s", r.hex())
+        return r
+
+    def ftp_ctrl0_get(self):
+        r = self.read(StUsb4500Register.Ctrl0, 1)
+        c = Ctrl0(r[0])
+        self.logger.debug("Ctrl0 > %s", repr(c))
+        return c
 
     def ftp_ctrl0_wait(self, to_clear = 0):
-        while self.read(CTRL_0_ADDR, 1)[0] & to_clear:
+        while self.ftp_ctrl0_get() & to_clear:
             pass 
 
-    def ftp_ctrl1(self, value = 0):
-        self.write(CTRL_1_ADDR, bytes([value]))
+    def ftp_ctrl1(self, op, sector = 0):
+        self.logger.debug("Ctrl1 < %s %s", repr(op), repr(sector))
+        self.write(StUsb4500Register.Ctrl1, bytes([int(op) | int(sector)]))
 
-    def ftp_cmd_exec(self, ctrl1, ctrl0_sector = 0):
-        self.ftp_ctrl1(ctrl1)
-        self.ftp_ctrl0((CTRL_0_PWR | CTRL_0_RST_N | CTRL_0_REQ) | (ctrl0_sector & CTRL_0_SECT))
-        self.ftp_ctrl0_wait(CTRL_0_REQ)
+    def ftp_cmd_exec(self, op, ctrl0_sector = 0, ctrl1_sector = 0):
+        self.ftp_ctrl1(op, ctrl1_sector)
+        self.ftp_ctrl0((Ctrl0.PWR | Ctrl0.RST_N | Ctrl0.REQ) | (ctrl0_sector & Ctrl0.SECT))
+        self.ftp_ctrl0_wait(Ctrl0.REQ)
         
     def cust_read_mode_enter(self):
         self.ftp_password(FTP_CUST_PASSWORD)
-        self.ftp_ctrl0(CTRL_0_PWR | CTRL_0_RST_N)
+        self.ftp_ctrl0(Ctrl0.PWR | Ctrl0.RST_N)
         self.ftp_ctrl0()
 
     def cust_write_mode_enter(self, erase_map = 0x1f):
         self.ftp_password(FTP_CUST_PASSWORD)
-        self.write(RW_BUFFER_ADDR, bytes([0]) * 8)
+        self.ftp_buffer_write(b"\x00" * 8)
         self.ftp_ctrl0(0)
-        self.ftp_ctrl0(CTRL_0_PWR | CTRL_0_RST_N)
+        self.ftp_ctrl0(Ctrl0.PWR | Ctrl0.RST_N)
 
-        self.ftp_cmd_exec(((erase_map << 3) & CTRL_1_SECT_MAP) | OP_WRITE_SER)
-        self.ftp_cmd_exec(OP_SOFT_PROG_SECTOR)
-        self.ftp_cmd_exec(OP_ERASE_SECTOR)
+        erase_map &= 0x1f
+        self.ftp_cmd_exec(Ctrl1Op.WRITE_SER, 0, Ctrl1Sector(erase_map << 3))
+        self.ftp_cmd_exec(Ctrl1Op.SOFT_PROG_SECTOR)
+        self.ftp_cmd_exec(Ctrl1Op.ERASE_SECTOR)
 
     def cust_test_mode_exit(self):
-        self.ftp_ctrl0(CTRL_0_RST_N)
+        self.ftp_ctrl0(Ctrl0.RST_N)
         self.ftp_ctrl1(0)
         self.ftp_password(0)
 
     def cust_sector_read(self, sector):
-        self.ftp_ctrl0(CTRL_0_PWR | CTRL_0_RST_N)
-        self.ftp_cmd_exec(OP_READ, sector)
-        r = self.read(RW_BUFFER_ADDR, 8)
+        self.ftp_ctrl0(Ctrl0.PWR | Ctrl0.RST_N)
+        self.ftp_cmd_exec(Ctrl1Op.READ, sector)
+        r = self.ftp_buffer_read()
         self.ftp_ctrl0()
         return r
 
     def cust_sector_write(self, sector, data):
-        self.write(RW_BUFFER_ADDR, data)
-        self.ftp_ctrl0(CTRL_0_PWR | CTRL_0_RST_N)
-        self.ftp_cmd_exec(OP_WRITE_PL)
-        self.ftp_cmd_exec(OP_PROG_SECTOR, sector)
+        self.ftp_buffer_write(data)
+        self.ftp_ctrl0(Ctrl0.PWR | Ctrl0.RST_N)
+        self.ftp_cmd_exec(Ctrl1Op.WRITE_PL)
+        self.ftp_cmd_exec(Ctrl1Op.PROG_SECTOR, sector)
 
     def customization_dump(self):
         self.cust_read_mode_enter()
-        for i in range(5):
+        for i, bf in enumerate([Bank0, Bank1, Bank2, Bank3, Bank4]):
             data = self.cust_sector_read(i)
-            print(f"Sector{i}: {data.hex()}")
+            v = int.from_bytes(data, "little")
+            print(f"Bank{i}:")
+            bf(all = v).dump_pretty(print)
         self.cust_test_mode_exit()
 
     def customization_set(self, sector_data):
